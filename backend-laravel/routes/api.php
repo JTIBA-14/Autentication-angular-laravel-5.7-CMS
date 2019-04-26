32c35bb405cefafe74526da5caa1710f4191149b
Javier Tibaduisa <javiertibaduisa@gmail.com>
2019-04-26 09:10:26 -0500

    commit inicial backend

---
+++ backend-laravel/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//Route::group(['prefix' => 'auth', ], function () {
+    Route::post('login', 'Admin\AuthController@login');
+//});
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::post('logout', 'Admin\AuthController@logout');
+    Route::post('refresh', 'Admin\AuthController@refresh');
+    Route::post('me', 'Admin\AuthController@me');
+});
